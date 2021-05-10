08c0c6f03ac6eefa1e90456e8d892262b3a3ac27
Надир Надирбеков <nadirbekovnadir@gmail.com>
2021-05-10 14:59:52 +0300

    Добавьте файлы проекта.

---
+++ VideoTranslator/VideoTranslator.Android/Services/CameraDevice.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VideoTranslator.Interfaces;
+using Android.Hardware.Camera2;
+using Java.Lang;
+
+namespace VideoTranslator.Droid.Services
+{
+    class CameraDevice : ICameraDevice
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        private string cameraId;
+        private CameraCharacteristics cameraCharacteristics;
+        private CameraCaptureSession cameraCaptureSession;
+        private CaptureRequest captureRequest;
+        private CaptureRequest.Builder builder;
+    }
+}
---
+++ VideoTranslator/VideoTranslator/Interfaces/ICameraDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoTranslator.Interfaces
+{
+    public interface ICameraDevice
+    {
+        public string Name { get; set; }
+    }
+}
