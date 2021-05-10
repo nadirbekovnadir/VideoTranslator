c00358d616c6e6a485a379e135634aab675fa033
Надир Надирбеков <nadirbekovnadir@gmail.com>
2021-05-10 15:09:53 +0300

    Добавил начальную структуру проекта. И немного кода для камеры

--- VideoTranslator/VideoTranslator.Android/Services/CameraDeviceFactory.cs
+++ VideoTranslator/VideoTranslator.Android/Services/CameraDeviceFactory.cs
@@ -10,11 +10,22 @@ using System.Linq;
 using System.Text;
 using VideoTranslator.Interfaces;
 using Android.Hardware.Camera2;
+using Java.Lang;
 
 namespace VideoTranslator.Droid.Services
 {
     class CameraDeviceFactory : ICameraDeviceFactory
     {
-        
+        private readonly CameraManager cameraManager;
+
+        public CameraDeviceFactory()
+        {
+            cameraManager = Application.Context.GetSystemService(Context.CameraService) as CameraManager;
+            if (cameraManager is null)
+            {
+                // Надо бы организовать что-то вроде окна лога
+                AndroidEnvironment.RaiseThrowable(new Throwable("CameraManager is null"));
+            }
+        }
     }
 }
